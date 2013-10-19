74ca0fe01a150505823cae88cfc851fc9970e95d
Vaibhav Sharma <vaibhavign@gmail.com>
2013-10-19 19:13:54 +0530

    change the url

--- woocommerce-pip-template/template-header.php
+++ woocommerce-pip-template/template-header.php
@@ -5,7 +5,7 @@
 	<title><?php _e('Print order details', 'woocommerce-pip'); ?></title>
 	<link href="<?php echo woocommerce_pip_template('uri', 'template.php'); ?>css/woocommerce-pip-print.css" rel=" stylesheet" type="text/css" media="print" />
 	<link href="<?php echo woocommerce_pip_template('uri', 'template.php'); ?>css/woocommerce-pip.css" rel=" stylesheet" type="text/css" media="screen,print" />
-        <script type="text/javascript" src="<?php echo WP_PLUGIN_URL  ?>/pips/js/jquery-1.3.2.min.js" ></script>  
-        <script type="text/javascript" src="<?php echo WP_PLUGIN_URL  ?>/pips/js/jquery-barcode.js" ></script> 
+        <script type="text/javascript" src="<?php echo WP_PLUGIN_URL  ?>/pip/js/jquery-1.3.2.min.js" ></script>  
+        <script type="text/javascript" src="<?php echo WP_PLUGIN_URL  ?>/pip/js/jquery-barcode.js" ></script> 
 </head>
 <body <?php if ($client != true) echo woocommerce_pip_preview(); ?>>
